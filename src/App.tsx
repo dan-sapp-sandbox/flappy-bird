00dbafe41e4a9d0affbe812dc14b8656fb334d40
Dan Sapp <dan.sapp@storable.com>
2025-03-03 18:25:35 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+"use client";
+import "./styles.css";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
+
+const FlappyBird = () => {
+  return (
+    <div className="ui">
+      <button
+        id="mute"
+        className="button-reset"
+        aria-pressed="false"
+        aria-label="Mute/Unmute"
+      >
+        <VolumeUpIcon
+          fontSize="large"
+          className="text-white unpressed"
+        />
+        <VolumeOffIcon fontSize="large" className="text-white pressed" />
+      </button>
+    </div>
+  );
+};
+
+export default FlappyBird;
